feb72ede3d67f210ef643ac82217d31a369e0fdc
Liang Ao <1912864304@qq.com>
2019-11-29 10:03:02 +0800

    create user

--- src/main/java/com/jbit/pojo/User.java
+++ src/main/java/com/jbit/pojo/User.java
@@ -3,5 +3,6 @@ package com.jbit.pojo;
 public class User {
     public static void main(String[] args) {
         System.out.println("hello"+110);
+        System.out.println("hello"+120);
     }
 }
